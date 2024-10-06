d3e637cc2a74eae98005923abfba20894c30378e
v.khaplak <v.khaplak@g.nsu.ru>
2024-10-06 22:38:19 +0700

    test1

---
+++ task-0/main.cpp
@@ -0,0 +1,19 @@
+
+#include "test1.h"
+
+int main(const int argc, char **argv) {
+    if (argc != 3) {
+        std::cerr << "insufficient data" << std::endl;
+    }
+    countWord obj1;
+    std::string inputFileName = argv[1];
+    std::string outputFileName = argv[2];
+    try {
+        obj1.readingAndCounting(inputFileName);
+        obj1.sortDataAndWriteToCSV(outputFileName);
+    } catch (std::exception &ex) {
+        std::cerr << ex.what() << std::endl;
+    }
+
+    return (0);
+}
